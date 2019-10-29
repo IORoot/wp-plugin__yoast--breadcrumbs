9613a3f83a55abb98a0bf8580c6fb6c40efd05d8
Andrew Pearson <andrewpearson@Wintermute.lan>
2019-10-29 10:33:15 +0000

    Initial Commit

---
+++ yoast_bradcrumbs.php
@@ -0,0 +1,55 @@
+<?php
+
+/*
+Plugin Name: _ANDYP - YOAST Breadcrumbs
+Plugin URI: http://londonparkour.com
+Description: Creates a shortcode [yoast_breadcrumbs] to insert into your webpage. 
+Version: 1.0
+Author: Andy Pearson
+Author URI: http://londonparkour.com
+*/
+
+
+class andyp_yoast_breadcrumbs {
+
+    /**
+     * __construct
+     *
+     * @return void
+     */
+    public function __construct(){
+
+        $this->create_shortcode();
+        return;
+    }
+
+    /**
+     * create_shortcode
+     *
+     * @return void
+     */
+    public function create_shortcode(){
+         add_shortcode( 'yoast_breadcrumbs', array( $this, 'render_shortcode' ) );
+    }
+
+    /**
+     * render_shortcode
+     *
+     * @param mixed $atts
+     * @param mixed $content
+     * @return void
+     */
+    public function render_shortcode($atts, $content = null){
+
+        if ( function_exists('yoast_breadcrumb') ) {
+            yoast_breadcrumb( '<p id="breadcrumbs">','</p>' );
+        }
+
+        return;
+
+    }
+
+
+}
+
+new andyp_yoast_breadcrumbs;
